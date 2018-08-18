3cb3dccf0940f3a569e2d09469c764755a9d9667
quietwar <hodari@hiddengeniusproject.org>
2018-08-17 21:21:43 -0700

    working color picker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
+
+  def current_user
+      @current_user ||= User.find_by(id: session[:user])
+  end
+
+  def prefered_color
+    return current_user.color if (current_user && current_user.color.present?)
+    '#fff'
+  end
 end
--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -4,10 +4,26 @@ import Proptypes from "prop-types"
 import { SketchPicker } from "react-color"
 
 class Colorpicker extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { selector: props.selector };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange = (color) => {
+		document.body.style.background = color.hex;
+		let elem = document.querySelector('#'+this.state.selector)
+		elem.value = color.hex;
+	}
+
 	render() {
-		return (<React.Fragment>
-					     <SketchPicker/>
-				    </React.Fragment>);
+		return (
+			<React.Fragment>
+		     <SketchPicker color={this.props.color}
+				 								onChange={this.handleChange} />
+	    </React.Fragment>
+		);
 	}
 }
 
